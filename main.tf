32a9bc52591b41d34a72ee8715e9a3f6a726a47a
Sahil Jain <mr.sahiljain14@gmail.com>
2023-09-25 12:50:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,16 @@
-# Configure AWS provider
 provider "aws" {
-  region = "ap-south-1" # Change to your desired region
+  region = "ap-south-1" 
 }
 
-# Generate a new SSH key pair
 resource "tls_private_key" "example" {
   algorithm = "RSA"
 }
 
-# Create an AWS key pair
 resource "aws_key_pair" "generated_key" {
-  key_name   = "aws_key" # Provide a name for your AWS key pair
+  key_name   = "aws_key"
   public_key = tls_private_key.example.public_key_openssh
 }
 
-# Create EC2 Instance
 resource "aws_instance" "example_server" {
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
@@ -24,7 +20,6 @@ resource "aws_instance" "example_server" {
     Name = "MyServer"
   }
 
-  # SSH connection information
   connection {
     type        = "ssh"
     user        = "ec2-user"
@@ -32,21 +27,20 @@ resource "aws_instance" "example_server" {
     host        = self.public_ip
   }
 
-  # Provisioner block to install Jenkins
   provisioner "remote-exec" {
     inline = [
-      "sudo yum update -y",      # Update package manager
-      "sudo yum install -y java", # Install Java (required for Jenkins)
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo", # Add Jenkins repository
-      "sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key", # Import Jenkins GPG key
-      "sudo yum install -y jenkins", # Install Jenkins
-      "sudo systemctl start jenkins", # Start Jenkins
-      "sudo systemctl enable jenkins" # Enable Jenkins to start on boot
+      "sudo yum update -y",      
+      "sudo yum install -y java", 
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo", 
+      "sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key", 
+      "sudo yum install -y jenkins", 
+      "sudo systemctl start jenkins", 
+      "sudo systemctl enable jenkins" 
     ]
   }
 }
 
-# Data blocks for external data sources
+
 data "external" "jenkins_status" {
   program = ["bash", "${path.module}/jenkins_status.sh", aws_instance.example_server.public_ip, tls_private_key.example.private_key_pem]
 }
@@ -55,22 +49,22 @@ data "external" "state_maintenance" {
   program = ["bash", "${path.module}/state_maintenance.sh"]
 }
 
-# Output to Display Server IP
+
 output "server_ip" {
   value = aws_instance.example_server.public_ip
 }
 
-# Output to Display Jenkins Status
+
 output "jenkins_status" {
   value = data.external.jenkins_status.result
 }
 
-# Output to Display State Maintenance Information
+
 output "state_maintenance" {
   value = data.external.state_maintenance.result
 }
 
-# Configure S3 Backend for Terraform State (if not already configured)
+
 terraform {
   backend "s3" {
     bucket = "sahilawsbucket"
